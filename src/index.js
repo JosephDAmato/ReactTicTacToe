48382e1cb5bb1d7d4b2446f2ba9277aea1af4f07
Joseph DAmato <Joseph.Matthew.DAmato@gmail.com>
2022-10-20 11:07:47 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,145 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+const Square = ({ takeTurn, id }) => {
+  const mark = ["O", "X", "+"];
+  // id is the square's number
+  // filled tells us if square has been filled
+  // tik tells us symbol in square (same as player)
+  // We call takeTurn to tell Parent we have filled the square
+  const [filled, setFilled] = React.useState(false);
+  const [tik, setTik] = React.useState(2);
+
+  return (
+    <button
+      onClick={() => {
+        setTik(takeTurn(id));
+        setFilled(true);
+        console.log(`Square: ${id} filled by player : ${tik}`);
+      }}
+    >
+      <h1>{mark[tik]}</h1>
+    </button>
+  );
+};
+
+const Board = () => {
+  // 1st player is X ie 1
+  // State keeps track of next player and gameState
+  const [player, setPlayer] = React.useState(1);
+  const [gameState, setGameState] = React.useState([]);
+  const [mounted, setMounted] = React.useState(true);
+ 
+  const win = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 5, 6],
+  ];
+  const checkForWinner = (gameState) => {
+    // get array of box id's
+    // can't be a winner in less than 5 turns
+    if (gameState.length < 5) return "No Winner Yet";
+    let p0 = gameState.filter((item) => {
+      if (item.player == 0) return item;
+    });
+    p0 = p0.map((item) => item.id);
+    let px = gameState.filter((item) => {
+      if (item.player == 1) return item;
+    });
+    px = px.map((item) => item.id);
+    if (p0 != null && px != null) {
+      var win0 = win.filter((item) => {
+        return isSuperset(new Set(p0), new Set(item));
+      });
+      var winX = win.filter((item) => {
+        return isSuperset(new Set(px), new Set(item));
+      });
+    }
+    if (win0.length > 0) return "Player O ";
+    else if (winX.length > 0) return "Player X ";
+    return "No Winner Yet";
+  };
+  // check if subset is in the set
+  function isSuperset(set, subset) {
+    for (let elem of subset) {
+      if (!set.has(elem)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  let status = `Winner is ${checkForWinner(gameState)}`;
+  console.log(`We hava a winner ${status}`);
+  
+
+  
+  const takeTurn = (id) => {
+    setGameState([...gameState, { id: id, player: player }]);
+    setPlayer((player + 1) % 2); // get next player
+    return player;
+  };
+
+  // Checking for Winner takes a bit of work
+// We use JavaScript Sets to check players choices
+// against winning combinations
+// Online there is more compact version but I prefer this one
+
+
+  function renderSquare(i) {
+    // use properties to pass callback function takeTurn to Child
+    return <Square takeTurn={takeTurn} id={i}></Square>;
+  }
+  const toggle = () => {
+    setMounted(!mounted)
+  }
+  return (
+    <div className="game-board">
+      <div className='grid-row'>
+        {mounted && renderSquare(0)}
+        {mounted && renderSquare(1)}
+        {mounted && renderSquare(2)}
+      </div>
+      <div className='grid-row'>
+        {mounted && renderSquare(3)}
+        {mounted && renderSquare(4)}
+        {mounted && renderSquare(5)}
+      </div>
+      <div className='grid-row'>
+        {mounted && renderSquare(6)}
+        {mounted && renderSquare(7)}
+        {mounted && renderSquare(8)}
+      </div>
+      <div id="info">
+        <button onClick={toggle}>
+        Rest Game
+        </button>
+        <h1>{status}</h1>
+      </div>
+    </div>
+  );
+};
+
+const Game = () => {
+  return (
+    <div className="game">
+      <Board></Board>
+    </div>
+  );
+};
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
+
+
+
